26d9a9841c6c3de639f14ba1e25155658434426e
SmalkiGroove <chapeau.g77@gmail.com>
2024-01-31 17:12:00 +0100

    fixes

--- game_data/lua/scripts/advmap/handlers/starting-armies.lua
+++ game_data/lua/scripts/advmap/handlers/starting-armies.lua
@@ -7,7 +7,7 @@ function ReplaceStartingArmy(hero)
     if STARTING_ARMIES[hero] then
         army = STARTING_ARMIES[hero]
     else
-        local faction = FACTION_TEXT[HEROES[hero]] print(faction)
+        local faction = FACTION_TEXT[HEROES[hero]]
         army = STARTING_ARMIES[faction]
     end
     local k, units, amounts = GetHeroArmySummary(hero)
