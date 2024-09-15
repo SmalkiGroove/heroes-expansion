16d30f9cd74b7552de03abc9155427f6f0cf9d87
SmalkiGroove <chapeau.g77@gmail.com>
2024-09-15 22:29:37 +0200

    mapobjects artifact colors

--- tools/other/generate-files.py
+++ tools/other/generate-files.py
@@ -11,6 +11,7 @@ with open(os.path.join(data_path, artifact_file_path), 'r') as artifacts_file:
     artifacts_data = xmltodict.parse(artifacts_file_content)
 
 tmp_dict = {
+    'ARTF_CLASS_GRAIL': '',
     'ARTF_CLASS_MINOR': '',
     'ARTF_CLASS_MAJOR': '/Effects/_(Effect)/Artefacts/General/Blue.xdb#xpointer(/Effect)',
     'ARTF_CLASS_RELIC': '/Effects/_(Effect)/Artefacts/General/Red.xdb#xpointer(/Effect)',
@@ -19,11 +20,12 @@ tmp_dict = {
 for artifact in artifacts_data["Table_DBArtifact_ArtifactEffect"]["objects"]["Item"]:
     if artifact["ID"] != "ARTIFACT_NONE":
         mapobj_artifact_path = str(artifact["obj"]["ArtifactShared"]["@href"]).split('#')[0]
-        for line in fileinput.input(os.path.join(data_path, mapobj_artifact_path), inplace=True):
+        print(data_path + mapobj_artifact_path)
+        for line in fileinput.input(data_path + mapobj_artifact_path, inplace=True):
             if line.find('<Effect href=') > -1:
-                print(f'	<Effect href="{tmp_dict[str(artifact["obj"]["Type"])]}"/>', end='\n')
+                print(f'	<Effect href="{tmp_dict[str(artifact["obj"]["Type"])]}"/>', end='')
             else:
-                print(line, end='\n')
+                print(line, end='')
 
         # if artifact["obj"]["Type"] == "ARTF_CLASS_RELIC":
         #     mapobject_file = str(artifact["obj"]["ArtifactShared"]["@href"])
