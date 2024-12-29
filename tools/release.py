7f46b8f65b97501cad81fd4e558f5d48ede8b417
Guillaume CHAPEAU <guillaume.chapeau@kanoma.fr>
2024-12-29 23:48:29 +0100

    release script

---
+++ tools/release.py
@@ -0,0 +1,39 @@
+import os
+import zipfile
+
+workdir = os.path.dirname(os.path.abspath(__file__))
+
+print("=== UPDATE VERSION ===")
+
+path_to_version_file = "../VERSION"
+path_to_version_txt = "../game_data/texts/UI/MainMenu2/Version.txt"
+
+with open(os.path.join(workdir, path_to_version_file), 'r') as version_file:
+    version = version_file.read()
+
+print(f"> current version : {version}")
+version_txt = f"<h3_bright>H5X mod\nVersion {version}"
+
+with open(os.path.join(workdir, path_to_version_txt), 'w') as version_file:
+    version_file.write(version_txt)
+
+
+pak_file = f"../h5x-{version}.pak"
+lang_file = f"../h5x-{version}-texts-EN.pak"
+
+game_data_path = "../game_data"
+data_dirs = ["characters","data","doc","interface","lua","maps"]
+
+path_prefix = len(os.path.abspath(game_data_path)) + 1
+
+print("=== CREATE PAK ===")
+with zipfile.ZipFile(os.path.join(workdir, pak_file), 'w') as pak:
+    print(f"> root dir : {os.path.abspath(game_data_path)}")
+    for datadir in data_dirs:
+        datadir_prefix = path_prefix + len(datadir) + 1
+        print(f"> add {datadir} in pak")
+        for root, dirs, files in os.walk(os.path.join(workdir, game_data_path, datadir)):
+            for file in files:
+                    file_path = os.path.join(os.path.abspath(root), file)
+                    arcname = file_path[datadir_prefix:]
+                    pak.write(file_path, arcname=arcname)
