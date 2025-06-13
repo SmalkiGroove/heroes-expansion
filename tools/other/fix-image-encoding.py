b550fe98b0ad58b540df573df11aa18e73f17742
SmalkiGroove <chapeau.g77@gmail.com>
2025-06-13 23:13:45 +0200

    script image encoding

---
+++ tools/other/fix-image-encoding.py
@@ -0,0 +1,15 @@
+from wand.image import Image
+import os
+
+def fix_image_encoding(image_path):
+    with Image(filename=image_path) as img:
+        img.options['dds:mipmaps'] = '0'
+        img.save(filename=image_path)
+
+interface_path = "../../game_data/interface"
+
+for root, subdir, files in os.walk(interface_path):
+    for file in files:
+        if file.endswith(".dds"):
+            file_path = os.path.join(root, file)
+            fix_image_encoding(file_path)
