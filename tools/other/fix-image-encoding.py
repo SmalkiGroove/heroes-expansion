155805497257ad7ce7a10cf1b1b4ff45ceaa694b
SmalkiGroove <chapeau.g77@gmail.com>
2025-06-13 23:30:05 +0200

    wip

--- tools/other/fix-image-encoding.py
+++ tools/other/fix-image-encoding.py
@@ -6,10 +6,11 @@ def fix_image_encoding(image_path):
         img.options['dds:mipmaps'] = '0'
         img.save(filename=image_path)
 
-interface_path = "../../game_data/interface"
+interface_path = "../../game_data/interface/UI/pwlscreen"
 
 for root, subdir, files in os.walk(interface_path):
     for file in files:
         if file.endswith(".dds"):
             file_path = os.path.join(root, file)
             fix_image_encoding(file_path)
+            # exit()
