e7763f617e1c1001cf33e85199689c3e95e43a1e
SmalkiGroove <chapeau.g77@gmail.com>
2025-08-04 13:40:35 +0200

    wip

--- tools/generators/generate-heroes-ui.py
+++ tools/generators/generate-heroes-ui.py
@@ -4,6 +4,9 @@ import xmltodict
 import re
 from jinja2 import Environment, FileSystemLoader
 
+debug = True
+dry_run = True
+
 root_text_path = "../../game_texts/texts-EN"
 heroes_xdb_path = "../../game_data/data/MapObjects"
 reftable_xdb_path = "../../game_data/data/GameMechanics/RefTables"
@@ -100,8 +103,13 @@ def spell_background_path(spell):
 def write_from_template(tpl_name, output_path, variables):
     tpl = jinja_env.get_template(tpl_name)
     rendered = tpl.render(variables)
-    with open(output_path, 'w') as out_file:
-        out_file.write(rendered)
+    if not dry_run:
+        with open(output_path, 'w') as out_file:
+            out_file.write(rendered)
+
+def log(msg):
+    if debug:
+        print(msg)
 
 def get_hero_lua_name(hero_id):
     with open(os.path.join(armies_lua_path, "game", "heroes.lua"), 'r') as heroes_lua:
@@ -171,10 +179,10 @@ icon_pos = [0, 100, 170, 240, 310, 380, 100, 170, 240, 310, 380]
 icon_line = [0, 14, 14, 14, 14, 14, 84, 84, 84, 84, 84]
 subfolders = ["class", "army", "spec", "skills", "spells"]
 stats = {
-    "Att": {"name": "Offence", "pos": 0},
-    "Def": {"name": "Defence", "pos": 80},
-    "Spp": {"name": "SpellPower", "pos": 160},
-    "Klg": {"name": "Knowledge", "pos": 240},
+    "Att": {"name": "Offence", "pos": 0, "prob": "OffenceProb"},
+    "Def": {"name": "Defence", "pos": 80, "prob": "DefenceProb"},
+    "Spp": {"name": "SpellPower", "pos": 160, "prob": "SpellpowerProb"},
+    "Klg": {"name": "Knowledge", "pos": 240, "prob": "KnowledgeProb"},
 }
 counter = 0
 
@@ -202,88 +210,89 @@ for folder,faction in factions.items():
             if not isinstance(hero_spells, list):
                 hero_spells = [hero_spells]
 
-            print(hero_id)
-            print(hero_class)
-            print(hero_name_file)
-            print(hero_face_file)
-            print(hero_skills)
-            print(hero_perks)
-            print(hero_spells)
+            log(hero_id)
+            log(hero_class)
+            log(hero_name_file)
+            log(hero_face_file)
+            log(hero_skills)
+            log(hero_perks)
+            log(hero_spells)
 
             hero_doc_path = hero_base_path(hero_id, faction)
             os.makedirs(hero_doc_path, exist_ok=True)
             for subf in subfolders:
                 os.makedirs(os.path.join(hero_doc_path, subf), exist_ok=True)
 
-            # write_from_template("herobutton.(WindowMSButton).xdb.j2", button_base_path(hero_id, faction), {'hero': hero_id, 'name_ref': hero_name_file})
-            # write_from_template("herobutton.(WindowMSButtonShared).xdb.j2", button_shared_path(hero_id, faction), {'hero': hero_id})
-            # write_from_template("windowbg.(BackgroundSimpleScallingTexture).xdb.j2", button_background_path(hero_id, faction), {'path': hero_face_file, 'size': 128})
-            # write_from_template("switch.(UISSendUIMessage).xdb.j2", button_switchon_path(hero_id, faction), {'hero': hero_id, 'open': 1, 'close': 0})
-            # write_from_template("switch.(UISSendUIMessage).xdb.j2", button_switchoff_path(hero_id, faction), {'hero': hero_id, 'open': 0, 'close': 1})
+            write_from_template("herobutton.(WindowMSButton).xdb.j2", button_base_path(hero_id, faction), {'hero': hero_id, 'name_ref': hero_name_file})
+            write_from_template("herobutton.(WindowMSButtonShared).xdb.j2", button_shared_path(hero_id, faction), {'hero': hero_id})
+            write_from_template("windowbg.(BackgroundSimpleScallingTexture).xdb.j2", button_background_path(hero_id, faction), {'path': hero_face_file, 'size': 128})
+            write_from_template("switch.(UISSendUIMessage).xdb.j2", hero_switchon_path(hero_id, faction), {'hero': hero_id, 'open': 1, 'close': 0})
+            write_from_template("switch.(UISSendUIMessage).xdb.j2", hero_switchoff_path(hero_id, faction), {'hero': hero_id, 'open': 0, 'close': 1})
             
-            # write_from_template("herowindow.(WindowSimple).xdb.j2", hero_mainwindow_path(hero_id, faction), {'hero': hero_id})
-            # write_from_template("herowindow.(WindowSimpleShared).xdb.j2", hero_mainwindowshared_path(hero_id, faction), {'hero': hero_id})
+            write_from_template("herowindow.(WindowSimple).xdb.j2", hero_mainwindow_path(hero_id, faction), {'hero': hero_id})
+            write_from_template("herowindow.(WindowSimpleShared).xdb.j2", hero_mainwindowshared_path(hero_id, faction), {'hero': hero_id})
 
-            # write_from_template("heroclass.(WindowSimple).xdb.j2", hero_classwindow_path(hero_id, faction), {'hero': hero_id})
-            # write_from_template("heroclass.(WindowSimpleShared).xdb.j2", hero_classwindowshared_path(hero_id, faction), {'hero': hero_id})
+            write_from_template("heroclass.(WindowSimple).xdb.j2", hero_classwindow_path(hero_id, faction), {'hero': hero_id})
+            write_from_template("heroclass.(WindowSimpleShared).xdb.j2", hero_classwindowshared_path(hero_id, faction), {'hero': hero_id})
             heroclass_data = get_class_data(hero_class)
             class_name_ref = os.path.join("/GameMechanics/RefTables", heroclass_data['NameFileRef']['@href'])
-            # write_from_template("heroclass.(WindowTextView).xdb.j2", hero_classwindowtext_path(hero_id, faction), {'hero': hero_id, 'class_ref': class_name_ref})
-            # write_from_template("herostat.(WindowSimple).xdb.j2", hero_statwindow_path(hero_id, faction), {'hero': hero_id})
-            # write_from_template("herostat.(WindowSimpleShared).xdb.j2", hero_statwindowshared_path(hero_id, faction), {'hero': hero_id})
-            # for stat,stat_data in stats.items():
-            #     write_from_template("herostatx.(WindowSimple).xdb.j2", hero_statxwindow_path(hero_id, faction, stat), {'hero': hero_id, 'stat': stat, 'name': stat_data['name'], 'pos': stat_data['pos']})
-            #     write_from_template("herostatx.(WindowTextView).xdb.j2", hero_statxtext_path(hero_id, faction, stat), {'hero': hero_id, 'stat': stat, 'pos': 36+stat_data['pos']})
+            write_from_template("heroclass.(WindowTextView).xdb.j2", hero_classtext_path(hero_id, faction), {'hero': hero_id, 'class_ref': class_name_ref})
+            write_from_template("herostat.(WindowSimple).xdb.j2", hero_statwindow_path(hero_id, faction), {'hero': hero_id})
+            write_from_template("herostat.(WindowSimpleShared).xdb.j2", hero_statwindowshared_path(hero_id, faction), {'hero': hero_id})
+            for stat,stat_data in stats.items():
+                statprob = heroclass_data['AttributeProbs'][stat_data['prob']]
+                write_from_template("herostatx.(WindowSimple).xdb.j2", hero_statxwindow_path(hero_id, faction, stat), {'hero': hero_id, 'stat': stat, 'name': stat_data['name'], 'pos': stat_data['pos']})
+                write_from_template("herostatx.(WindowTextView).xdb.j2", hero_statxtext_path(hero_id, faction, stat), {'hero': hero_id, 'stat': stat, 'pos': 36+stat_data['pos'], 'value': statprob})
 
-            # write_from_template("heroarmy.(WindowSimple).xdb.j2", hero_armywindow_path(hero_id, faction), {'hero': hero_id})
+            write_from_template("heroarmy.(WindowSimple).xdb.j2", hero_armywindow_path(hero_id, faction), {'hero': hero_id})
             hero_var = get_hero_lua_name(hero_id)
             if hero_var:
                 hero_army = get_hero_starting_army(hero_var, faction)
                 if hero_army:
                     hero_army = hero_army.replace('{','').replace('}','').replace(' ','').split(',')
-                    print(hero_army)
-                    # write_from_template("heroarmy.(WindowSimpleShared).xdb.j2", hero_armywindowshared_path(hero_id, faction), {'hero': hero_id, 'nb': 3})
+                    log(hero_army)
+                    write_from_template("heroarmy.(WindowSimpleShared).xdb.j2", hero_armywindowshared_path(hero_id, faction), {'hero': hero_id, 'nb': 3})
 
-            # write_from_template("heroskills.(WindowSimple).xdb.j2", hero_skillswindow_path(hero_id, faction), {'hero': hero_id})
-            # write_from_template("heroskills.(WindowSimpleShared).xdb.j2", hero_skillswindowshared_path(hero_id, faction), {'hero': hero_id, 'nb': len(hero_skills)+len(hero_perks)})
+            write_from_template("heroskills.(WindowSimple).xdb.j2", hero_skillswindow_path(hero_id, faction), {'hero': hero_id})
+            write_from_template("heroskills.(WindowSimpleShared).xdb.j2", hero_skillswindowshared_path(hero_id, faction), {'hero': hero_id, 'nb': len(hero_skills)+len(hero_perks)})
             counter = 0
             for skill in hero_skills:
                 counter += 1
                 skill_mastery = masteries[skill['Mastery']]
                 skill_id = skill['SkillID']
                 skill_uid = f"{skill_id}_{skill_mastery}"
-                print(skill_id)
+                log(skill_id)
                 skill_data = get_skill_data(skill_id)
                 skill_name_file = skill_data['NameFileRef']['Item'][skill_mastery-1]['@href']
                 skill_texture_path = skill_data['Texture']['Item'][skill_mastery]['@href']
-                print(skill_name_file)
-                print(skill_texture_path)
-                # write_from_template("heroskillx.(WindowSimple).xdb.j2", hero_skillxwindow_path(hero_id, faction, counter), {'hero': hero_id, 'skill': skill_uid, 'x': counter, 'pos': icon_pos[counter], 'name_ref': skill_name_file})
-                # write_from_template("windowshared.(WindowSimpleShared).xdb.j2", skill_windowshared_path(skill_uid), {'id': skill_uid})
-                # write_from_template("windowbg.(BackgroundSimpleScallingTexture).xdb.j2", skill_background_path(skill_uid), {'path': skill_texture_path, 'size': 64})
+                log(skill_name_file)
+                log(skill_texture_path)
+                write_from_template("heroskillx.(WindowSimple).xdb.j2", hero_skillxwindow_path(hero_id, faction, counter), {'hero': hero_id, 'skill': skill_uid, 'x': counter, 'pos': icon_pos[counter], 'name_ref': skill_name_file})
+                write_from_template("windowshared.(WindowSimpleShared).xdb.j2", skill_windowshared_path(skill_uid), {'id': skill_uid})
+                write_from_template("windowbg.(BackgroundSimpleScallingTexture).xdb.j2", skill_background_path(skill_uid), {'path': skill_texture_path, 'size': 64})
             for perk in hero_perks:
                 counter += 1
-                print(perk)
+                log(perk)
                 perk_data = get_skill_data(perk)
                 perk_name_file = perk_data['NameFileRef']['Item']['@href']
                 perk_texture_path = perk_data['Texture']['Item'][1]['@href']
-                print(perk_name_file)
-                print(perk_texture_path)
-                # write_from_template("heroskillx.(WindowSimple).xdb.j2", hero_skillxwindow_path(hero_id, faction, counter), {'hero': hero_id, 'skill': perk, 'x': counter, 'pos': icon_pos[counter], 'name_ref': perk_name_file})
-                # write_from_template("windowshared.(WindowSimpleShared).xdb.j2", skill_windowshared_path(perk), {'id': perk})
-                # write_from_template("windowbg.(BackgroundSimpleScallingTexture).xdb.j2", skill_background_path(perk), {'path': perk_texture_path, 'size': 64})
+                log(perk_name_file)
+                log(perk_texture_path)
+                write_from_template("heroskillx.(WindowSimple).xdb.j2", hero_skillxwindow_path(hero_id, faction, counter), {'hero': hero_id, 'skill': perk, 'x': counter, 'pos': icon_pos[counter], 'name_ref': perk_name_file})
+                write_from_template("windowshared.(WindowSimpleShared).xdb.j2", skill_windowshared_path(perk), {'id': perk})
+                write_from_template("windowbg.(BackgroundSimpleScallingTexture).xdb.j2", skill_background_path(perk), {'path': perk_texture_path, 'size': 64})
 
-            # write_from_template("herospells.(WindowSimple).xdb.j2", hero_spellswindow_path(hero_id, faction), {'hero': hero_id})
-            # write_from_template("herospells.(WindowSimpleShared).xdb.j2", hero_spellswindowshared_path(hero_id, faction), {'hero': hero_id, 'nb': len(hero_spells)})
+            write_from_template("herospells.(WindowSimple).xdb.j2", hero_spellswindow_path(hero_id, faction), {'hero': hero_id})
+            write_from_template("herospells.(WindowSimpleShared).xdb.j2", hero_spellswindowshared_path(hero_id, faction), {'hero': hero_id, 'nb': len(hero_spells)})
             counter = 0
             for spell in hero_spells:
                 counter += 1
-                print(spell)
+                log(spell)
                 spell_data = get_spell_data(spell)
                 spell_name_file = spell_data['NameFileRef']['@href']
                 spell_texture_path = spell_data['Texture']['@href']
-                print(spell_name_file)
-                print(spell_texture_path)
-                # write_from_template("herospellx.(WindowSimple).xdb.j2", hero_spellxwindow_path(hero_id, faction, counter), {'hero': hero_id, 'spell': spell, 'x': counter, 'pos': icon_pos[counter], 'line': icon_line[counter], 'name_ref': spell_name_file})
-                # write_from_template("windowshared.(WindowSimpleShared).xdb.j2", spell_windowshared_path(spell), {'id': spell})
-                # write_from_template("windowbg.(BackgroundSimpleScallingTexture).xdb.j2", spell_background_path(spell), {'path': spell_texture_path, 'size': 128})
+                log(spell_name_file)
+                log(spell_texture_path)
+                write_from_template("herospellx.(WindowSimple).xdb.j2", hero_spellxwindow_path(hero_id, faction, counter), {'hero': hero_id, 'spell': spell, 'x': counter, 'pos': icon_pos[counter], 'line': icon_line[counter], 'name_ref': spell_name_file})
+                write_from_template("windowshared.(WindowSimpleShared).xdb.j2", spell_windowshared_path(spell), {'id': spell})
+                write_from_template("windowbg.(BackgroundSimpleScallingTexture).xdb.j2", spell_background_path(spell), {'path': spell_texture_path, 'size': 128})
