3d3ccdf3b05b2c258afc0cd7221d1914dd54b8bd
SmalkiGroove <chapeau.g77@gmail.com>
2025-09-16 13:48:28 +0200

    Revert "remove textures mipmaps" (partiel)

---
+++ tools/other/fix-image-encoding.py
@@ -0,0 +1,19 @@
+from wand.image import Image
+from wand import image
+import os
+
+def fix_image_encoding(image_path):
+    with Image(filename=image_path) as img:
+        # img.options['dds:mipmaps'] = '0'
+        # img.compression = 'dxt3'
+        image.library.MagickSetOption(img.wand, b'dds:mipmaps', b'0')
+        img.save(filename=image_path)
+
+interface_path = "../../game_data/interface/UI/pwlscreen"
+
+for root, subdir, files in os.walk(interface_path):
+    for file in files:
+        if file.endswith(".dds"):
+            file_path = os.path.join(root, file)
+            fix_image_encoding(file_path)
+            # exit()
